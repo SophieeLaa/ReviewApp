476f5c3b4fb4a9e2a6d3c96512c8c7c98901ff74
sophie 888 <sophie88998899@gmail.com>
2024-01-10 11:28:44 +0400

    Adding migrations

--- ReviewApp/Program.cs
+++ ReviewApp/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectin"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 var app = builder.Build();
