e1d74f0bb320e9eb4b58112332d6bb6c4aca2a44
sophie 888 <sophie88998899@gmail.com>
2024-01-10 11:00:20 +0400

    Add project files.

---
+++ ReviewApp/Models/Country.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Principal;
+
+namespace ReviewApp.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Owner> Owners { get; set; }
+    }
+}
